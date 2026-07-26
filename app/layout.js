e2e056a6510e974c1231c3c2bdd139d3719c3d48
Jayden Akpalu <akpalujayden@gmail.com>
2026-07-26 15:29:55 +0000

    fix analytics import path typo

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import localFont from 'next/font/local'
 import './globals.css'
-import { Analytics } from '@/vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/react'
 
 const cooperHewitt = localFont({
   src: [
